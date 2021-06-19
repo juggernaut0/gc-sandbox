ed1266c9728f6c9ed23dcbf1dc5db334e36dd234
Tom Warner <tomwamt@gmail.com>
2021-06-19 00:34:21 -0600

    Tracing and collection

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,35 +1,53 @@
 use gc::*;
 use std::mem::size_of;
 
-#[derive(GcNew)]
+#[derive(GcNew, Debug)]
 struct A {
     b: GcPtr<B>,
 }
 
+unsafe impl Trace for A {
+    fn trace(&self, tracer: &mut Tracer) {
+        self.b.trace(tracer);
+    }
+}
+
+#[derive(Debug)]
 struct B {
     i: i32
 }
 
+unsafe impl Trace for B {
+    fn trace(&self, tracer: &mut Tracer) {
+        self.i.trace(tracer);
+    }
+}
+
 fn main() {
+    dbg!(size_of::<A>());
+    dbg!(size_of::<B>());
+    dbg!(size_of::<GcPtr<A>>());
+    dbg!(size_of::<GcBor<A>>());
+    dbg!(size_of::<GcRoot<A>>());
+
     let gc = Gc::new();
+    gc.stats();
 
     let ctx: GcContext = gc.context();
     let b: GcBor<B> = ctx.allocate(B { i: 42 });
+    let b2: GcBor<B> = ctx.allocate(B { i: 69 });
     let a: GcBor<A> = A::gc_new(&ctx, b);
     let a_root: GcRoot<A> = gc.root(a);
-    println!("{}", b.i);
+    dbg!(b);
+    dbg!(b2);
 
-    /*let ctx2: GcContext = gc.context();
-    let b2: GcBor<B> = ctx2.allocate(B { i: 69 });*/
-
+    gc.stats();
     ctx.collect();
+    gc.stats();
 
-    //println!("{}", b2.i);
+    // compile error
+    // dbg!(b2);
 
     let new_ctx = gc.context();
-    println!("{}", a_root.borrow(&new_ctx).b.i);
-
-    dbg!(size_of::<GcPtr<i64>>());
-    dbg!(size_of::<GcBor<i64>>());
-    dbg!(size_of::<GcRoot<i64>>());
+    dbg!(a_root.borrow(&new_ctx));
 }
--- gc/src/lib.rs
+++ gc/src/lib.rs
@@ -3,6 +3,8 @@ use std::marker::PhantomData;
 use std::cell::{RefCell, RefMut};
 
 pub use gc_derive::*;
+use std::fmt::{Debug, Formatter};
+use std::collections::{HashMap, HashSet};
 
 pub struct GcPtr<T> {
     ptr: *const T,
@@ -11,6 +13,12 @@ pub struct GcPtr<T> {
 impl<T> GcPtr<T> {
     pub unsafe fn from_bor(bor: GcBor<T>) -> GcPtr<T> {
         GcPtr { ptr: bor.ptr }
+    }
+}
+
+impl<T: Debug> Debug for GcPtr<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.deref())
     }
 }
 
@@ -35,6 +43,12 @@ impl<'ctx, 'gc, T> GcBor<'ctx, 'gc, T> {
     }
 }
 
+impl<T: Debug> Debug for GcBor<'_, '_, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.deref())
+    }
+}
+
 impl<'ctx, 'gc, T> Deref for GcBor<'ctx, 'gc, T> {
     type Target = T;
 
@@ -52,26 +66,33 @@ impl<'ctx, 'gc, T> Clone for GcBor<'ctx, 'gc, T> {
 
 impl<'ctx, 'gc, T> Copy for GcBor<'ctx, 'gc, T> {}
 
-pub struct GcRoot<'gc, T> {
+pub struct GcRoot<'gc, T: Trace + 'static> {
     ptr: *const T,
-    //gc: &'gc Gc,
-    phantom: PhantomData<&'gc Gc>,
+    gc: &'gc Gc,
 }
 
-impl<'gc, T> GcRoot<'gc, T> {
+impl<'gc, T: Trace> GcRoot<'gc, T> {
     pub fn borrow<'ctx>(&self, ctx: &'ctx GcContext<'gc>) -> GcBor<'ctx, 'gc, T> {
         // safety: self.ptr cannot be constructed by user code and is guaranteed by module to be init and valid
         GcBor::new(unsafe { &*self.ptr }, ctx)
     }
 }
 
+impl<T: Trace + 'static> Drop for GcRoot<'_, T> {
+    fn drop(&mut self) {
+        self.gc.unroot(self.ptr)
+    }
+}
+
 pub struct Gc {
     context_ref: RefCell<()>,
+    objs: RefCell<HashMap<*mut dyn Trace, bool>>,
+    roots: RefCell<HashSet<*mut dyn Trace>>,
 }
 
 pub struct GcContext<'gc> {
-    r: RefMut<'gc, ()>,
-    phantom: PhantomData<&'gc Gc>
+    _r: RefMut<'gc, ()>,
+    gc: &'gc Gc,
 }
 
 #[derive(Debug)]
@@ -79,12 +100,22 @@ pub struct GcContextError;
 
 impl Gc {
     pub fn new() -> Gc {
-        Gc { context_ref: RefCell::new(()) }
+        Gc {
+            context_ref: RefCell::default(),
+            objs: RefCell::default(),
+            roots: RefCell::default(),
+        }
+    }
+
+    pub fn stats(&self) {
+        eprintln!("objs: {}", self.objs.borrow().len());
+        eprintln!("roots: {}", self.roots.borrow().len());
+        eprintln!("size: {}", self.objs.borrow().iter().map(|(ptr, _)| unsafe { std::mem::size_of_val(&**ptr) }).sum::<usize>());
     }
 
     pub fn try_context(&self) -> Result<GcContext, GcContextError> {
         match self.context_ref.try_borrow_mut() {
-            Ok(r) => Ok(GcContext { r, phantom: PhantomData::default() }),
+            Ok(r) => Ok(GcContext { _r: r, gc: self }),
             Err(_) => Err(GcContextError)
         }
     }
@@ -93,17 +124,90 @@ impl Gc {
         self.try_context().expect("Context already exists")
     }
 
-    pub fn root<'gc, T>(&'gc self, bor: GcBor<T>) -> GcRoot<'gc, T> {
-        GcRoot { ptr: bor.ptr, phantom: PhantomData::default() } // TODO store for tracing later
+    pub fn root<'gc, T: Trace + 'static>(&'gc self, bor: GcBor<T>) -> GcRoot<'gc, T> {
+        let ptr: *mut dyn Trace = bor.ptr as *mut T;
+        self.roots.borrow_mut().insert(ptr);
+        GcRoot { ptr: bor.ptr, gc: self } // TODO store for tracing later
+    }
+
+    fn unroot<T: Trace + 'static>(&self, ptr: *const T) {
+        let ptr: *mut dyn Trace = ptr as *mut T;
+        self.roots.borrow_mut().remove(&ptr);
+    }
+
+    fn allocate<T: Trace + 'static>(&self, ptr: *mut T) -> *const T {
+        self.objs.borrow_mut().insert(ptr, false);
+        ptr
+    }
+
+    fn collect(&self) {
+        let mut objs = self.objs.borrow_mut();
+        unsafe {
+            for ptr in self.roots.borrow().iter() {
+                objs.insert(*ptr, true);
+            }
+            let mut tracer = Tracer { objs: &mut objs };
+            for ptr in self.roots.borrow().iter() {
+                (&mut **ptr).trace(&mut tracer)
+            }
+
+            objs.retain(|ptr, marked| {
+                if *marked {
+                    *marked = false;
+                    true
+                } else {
+                    drop(Box::from_raw(*ptr));
+                    false
+                }
+            });
+        }
     }
 }
 
 impl<'gc> GcContext<'gc> {
-    pub fn allocate<'ctx, T>(&'ctx self, t: T) -> GcBor<'ctx, 'gc, T> {
-        GcBor::new(Box::into_raw(Box::new(t)), self) // TODO store for collection later
+    pub fn allocate<'ctx, T: Trace + 'static>(&'ctx self, t: T) -> GcBor<'ctx, 'gc, T> {
+        GcBor::new(self.gc.allocate(Box::into_raw(Box::new(t))), self)
     }
 
     pub fn collect(self) {
-        // TODO
+        self.gc.collect()
+    }
+}
+
+// unsafe if manually implemented
+pub unsafe trait Trace {
+    fn trace(&self, tracer: &mut Tracer);
+}
+
+unsafe impl<T: Trace + 'static> Trace for GcPtr<T> {
+    fn trace(&self, tracer: &mut Tracer) {
+        tracer.mark(self.ptr);
+        self.deref().trace(tracer);
+    }
+}
+
+macro_rules! noop_trace {
+    ($t: ty) => {
+        unsafe impl Trace for $t {
+            fn trace(&self, _tracer: &mut Tracer) {
+                // noop
+            }
+        }
+    };
+}
+
+noop_trace!(i32);
+noop_trace!(i64);
+
+pub struct Tracer<'a> {
+    objs: &'a mut HashMap<*mut dyn Trace, bool>
+}
+
+impl Tracer<'_> {
+    fn mark<T: Trace + 'static>(&mut self, ptr: *const T) {
+        let ptr: *mut dyn Trace = ptr as *mut T;
+        if self.objs.contains_key(&ptr) {
+            self.objs.insert(ptr, true);
+        }
     }
 }
